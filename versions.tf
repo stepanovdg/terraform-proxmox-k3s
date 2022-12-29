a399539ad9cf802768c57541e5e8a58e7d70d549
sedos <stepanov.dg+sedos@gmail.com>
2022-12-29 00:33:00 -0800

    13.1 and without support node

--- versions.tf
+++ versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.3"
     }
 
     macaddress = {
--- worker_nodes.tf
+++ worker_nodes.tf
@@ -8,20 +8,10 @@ locals {
     for pool in var.node_pools :
     [
       for i in range(pool.size) :
-      merge(defaults(pool, {
-        cores          = 2
-        sockets        = 1
-        memory         = 4096
-        storage_type   = "scsi"
-        storage_id     = "local-lvm"
-        disk_size      = "20G"
-        user           = "k3s"
-        template       = var.node_template
-        network_bridge = "vmbr0"
-        network_tag    = -1
-        }), {
-        i  = i
-        ip = cidrhost(pool.subnet, i)
+      merge(pool, {
+        i        = i
+        ip       = cidrhost(pool.subnet, i)
+        template = coalesce(pool.template, var.node_template)
       })
     ]
   ])
@@ -34,7 +24,7 @@ locals {
 
 resource "proxmox_vm_qemu" "k3s-worker" {
   depends_on = [
-    proxmox_vm_qemu.k3s-support,
+    local_file.k3s_nginx_config,
     proxmox_vm_qemu.k3s-master,
   ]
 
